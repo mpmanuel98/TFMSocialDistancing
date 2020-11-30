e0affcbf893ce9fb29c2bb2cf63f6b03164c61a7
Manuel <mpmanuel98@gmail.com>
2020-11-30 13:31:41 +0100

    Update

---
+++ modules/foscam_webcams.py
@@ -0,0 +1,472 @@
+"""
+Module foscam_webcams.py.
+
+In this module there are some definitions ir order to send requests to
+the Foscam IP cameras installed on the smart home. With that, the user 
+will be able to move the cameras, move to a preset point, zoom in,
+zoom out and take snaps.
+"""
+
+__version__ = "1.0"
+__author__ = "Manuel Marín Peral"
+
+import io
+import xml.etree.ElementTree as ET
+
+import requests
+from PIL import Image
+
+"""
+Attributes
+----------
+Authentication credentials.
+Access URLs to the cameras.
+Dictionaries with the requests parameters.
+"""
+
+user = "admin"
+password = "AmgCam18*"
+
+url_living_room = "http://192.168.7.225:8894/cgi-bin/CGIProxy.fcgi?"
+url_distributor = "http://192.168.7.224:8893/cgi-bin/CGIProxy.fcgi?"
+url_kitchen = "http://192.168.7.223:8892/cgi-bin/CGIProxy.fcgi?"
+url_bedroom = "http://192.168.7.222:8891/cgi-bin/CGIProxy.fcgi?"
+url_home_tests = "http://192.168.1.50:88/cgi-bin/CGIProxy.fcgi?"
+
+params_zoom_in = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "zoomIn"
+}
+
+params_zoom_out = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "zoomOut"
+}
+
+params_zoom_stop = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "zoomStop"
+}
+
+params_movement_stop = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzStopRun"
+}
+
+params_move_up = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveUp"
+}
+
+params_move_down = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveDown"
+}
+
+params_move_left = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveLeft"
+}
+
+params_move_right = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveRight"
+}
+
+params_move_top_left = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveTopLeft"
+}
+
+params_move_top_right = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveTopRight"
+}
+
+params_move_bottom_left = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveBottomLeft"
+}
+
+params_move_bottom_right = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "ptzMoveBottomRight"
+}
+
+params_dev_state = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "getDevState"
+}
+
+params_get_config_motion_detect = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "getMotionDetectConfig1"
+}
+
+params_take_snap = {
+    "usr": user,
+    "pwd": password,
+    "cmd": "snapPicture2"
+}
+
+"""
+Definitions (functions)
+----------
+"""
+
+def get_motion_detection_alarm(camera_url):
+    """Gets the motion detection alarm.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Motion detection disabled
+        1: No movement detected
+        2: Movement detected
+    """
+
+    response = requests.get(camera_url, params=params_dev_state)
+    response_xml = ET.fromstring(response.text)
+    motion_detect_alarm = int(response_xml[2].text)
+
+    return motion_detect_alarm
+
+def zoom_in(camera_url):
+    """Zooms into the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_zoom_in)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def zoom_out(camera_url):
+    """Zooms out on the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_zoom_out)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def zoom_stop(camera_url):
+    """Stops zooming on the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_zoom_stop)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def movement_stop(camera_url):
+    """Stops the movement on the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_movement_stop)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_up(camera_url):
+    """Moves up the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_up)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_down(camera_url):
+    """Moves down the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_down)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_left(camera_url):
+    """Moves the specified camera to the left.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_left)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_right(camera_url):
+    """Moves the specified camera to the right.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_right)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_top_left(camera_url):
+    """Moves the specified camera to the top-left position.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_top_left)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_top_right(camera_url):
+    """Moves the specified camera to the top-right position.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_top_right)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_bottom_left(camera_url):
+    """Moves the specified camera to the bottom-left position.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_bottom_left)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_bottom_right(camera_url):
+    """Moves the specified camera to the bottom-right position.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    response = requests.get(camera_url, params=params_move_bottom_right)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def move_preset_point(camera_url, preset_point):
+    """Moves the specified camera to a specified preset point.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+    preset_point : string
+        Destination preset point.
+
+    Returns
+    -------
+    int
+        0: Success.
+        -1: Error.
+    """
+
+    params_move_preset_point = {"usr": user,
+                                "pwd": password,
+                                "cmd": "ptzGotoPresetPoint",
+                                "name": preset_point
+                                }
+
+    response = requests.get(camera_url, params=params_move_preset_point)
+    response_xml = ET.fromstring(response.text)
+    status_code = int(response_xml[0].text)
+
+    return status_code
+
+def take_capture(camera_url):
+    """Takes a capture on the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+
+    Returns
+    -------
+    Jpeg image data
+        Data corresponding to the capture taken.
+    """
+
+    response = requests.get(camera_url, params=params_take_snap)
+
+    return response.content
+
+def take_and_save_capture(camera_url, dir_dest):
+    """Takes and saves a capture on the specified camera.
+
+    Parameters
+    ----------
+    camera_url : string
+        URL of the source camera.
+    dir_dest : string
+        Directory where the capture will be saved.
+
+    Returns
+    -------
+    int
+        Response status code (200 = Success).
+    """
+
+    response = requests.get(camera_url, params=params_take_snap)
+    pil_image = Image.open(io.BytesIO(response.content))
+    pil_image.save(dir_dest)
+
+    return response.status_code
---
+++ modules/ocv_face_processing.py
@@ -0,0 +1,226 @@
+"""
+Module ocv_face_processing.py.
+
+In this module there are some definitions ir order to detect faces and
+to create the necessary structures to make the subsequent face
+recognition. Also, there is a Class to create Recognizer instances using
+a specified algorithm. For all the face processes OpenCV algorithms
+are used.
+
+Also some functions are defined:
+    detect_faces(img)
+    create_recognition_structures(training_images_path)
+"""
+
+__version__ = "1.0"
+__author__ = "Manuel Marín Peral"
+
+import os
+
+import cv2
+import numpy as np
+
+"""
+Definitions (functions)
+----------
+"""
+
+def detect_faces(img):
+    """Detects faces in a given image.
+
+    Parameters
+    ----------
+    img : Jpeg image data
+        Data corresponding to the image where faces are being detected.
+
+    Returns
+    -------
+    list
+        List of cropped and scaled images with the region of the
+        detected faces.
+    """
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Ecualizacion del histograma (para suavizar los cambios de iluminacion)
+    # equ = cv2.equalizeHist(gray)
+
+    # Aplicando filtro bilatreal para realizar un suavizado de la imagen
+    #blur = cv2.bilateralFilter(equ,9,75,75)
+
+    # Haar Classifier
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
+
+    faces_detected = face_cascade.detectMultiScale(gray, scaleFactor=1.2)
+
+    if (len(faces_detected) == 0):
+        return None
+
+    faces_info = []
+
+    for face in faces_detected:
+
+        x, y, w, h = face
+
+        cropped_scaled_face = cv2.resize(gray[y:y+h, x:x+w], (200, 200))
+
+        cv2.imshow("Detected face:", cropped_scaled_face)
+        cv2.waitKey(300)
+
+        faces_info.append(cropped_scaled_face)
+
+    return faces_info
+
+def create_recognition_structures(training_images_path):
+    """Creates the structures necessary to make the subsequent
+    face recognition.
+
+    Parameters
+    ----------
+    training_images_path : string
+        Path to the directory where the training images are saved.
+
+    Returns
+    -------
+    list
+        List of cropped and scaled images with the region of the
+        detected faces in the training samples.
+    list
+        List of integers which represents the label of each image
+        saved in the first returned list.
+    dict
+        Dictionary that contains the relation between the labels 
+        of the second returned list with the names of the people.
+    """
+
+    faces = []
+    labels = []
+    subject_names = dict()
+    subject_index = 0
+    directories = os.listdir(training_images_path)
+
+    for dir_name in directories:
+
+        subject_names[subject_index] = dir_name
+
+        subject_dir_path = training_images_path + "/" + dir_name
+        subject_images_names = os.listdir(subject_dir_path)
+
+        for image_name in subject_images_names:
+            
+            print("Processing the image: ", image_name)
+
+            image_path = subject_dir_path + "/" + image_name
+            image = cv2.imread(image_path)
+            detected_faces = detect_faces(image)
+
+            if detected_faces is None:
+                continue
+            else:
+                for face in detected_faces:
+                    faces.append(face)
+                    labels.append(subject_index)
+
+        subject_index += 1            
+
+    return faces, labels, subject_names
+
+"""
+Classes
+----------
+"""
+
+class Recognizer:
+    """
+    A class used to represent a face recognizer.
+
+    Attributes
+    ----------
+    recognizer : openCV.recognizer
+        The openCV face recognizer that is going to be used.
+    faces : list
+        List of cropped and scaled images with the region of the
+        detected faces in the training samples.
+    labels : list
+        List of integers which represents the label of each image
+        saved in the first returned list.
+    names : dict
+        Dictionary that contains the relation between the labels 
+        of the second returned list with the names of the people.
+
+    Methods
+    -------
+    __init__(recognizer, faces, labels, names)
+        Initialize an instance of the class.
+    predict(img)
+        Tries to recognize a person in the given image.
+    """
+    
+    recognizer = None
+    faces=[]
+    labels=[]
+    names=dict()
+
+    def __init__(self, recognizer, faces, labels, names):
+        """
+        Parameters
+        ----------
+        recognizer : string
+            The openCV face recognizer that is going to be used.
+        faces : list
+            List of cropped and scaled images with the region of the
+            detected faces in the training samples.
+        labels : list
+            List of integers which represents the label of each image
+            saved in the first returned list.
+        names : dict
+            Dictionary that contains the relation between the labels 
+            of the second returned list with the names of the people.
+        """
+
+        self.names = names
+
+        if(recognizer == "eigenfaces"):
+            self.recognizer = cv2.face.EigenFaceRecognizer_create()
+        elif(recognizer == "fisherfaces"):
+            self.recognizer = cv2.face.FisherFaceRecognizer_create()
+        else:
+            self.recognizer = cv2.face.LBPHFaceRecognizer_create(radius=1, neighbors=8, grid_x=8, grid_y=8)
+            
+        self.recognizer.train(faces, np.array(labels))   
+
+    def predict(self, img):
+        """Tries to recognize a person in the given image.
+
+        Parameters
+        ----------
+        img : Jpeg image data
+            Data corresponding to the image where faces are being
+            recognized.
+
+        Returns
+        -------
+        list
+            List with the name and the confidence of the people
+            recognized.
+        """
+
+        face_list = detect_faces(img)
+
+        if face_list is None:
+            return None
+
+        people_identified = []
+
+        for face in face_list:
+
+            info_recognizer = self.recognizer.predict(face)
+
+            person = []
+            label_text = self.names.get(info_recognizer[0])
+            person.append(label_text)
+            person.append(info_recognizer[1])
+
+            people_identified.append(person)
+
+        return people_identified
