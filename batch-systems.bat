07bc9c708091b14058f53511df123ba388933848
Manuel <mpmanuel98@gmail.com>
2021-05-19 19:17:32 +0200

    adding batch

---
+++ batch-systems.bat
@@ -0,0 +1,3 @@
+call "C:\Users\mpman\AppData\Local\Programs\Python\Python39\python.exe" C:\main_attendance.py -n "Cloud Computing" -c 71142104
+call "C:\Users\mpman\AppData\Local\Programs\Python\Python39\python.exe" C:\main_social_distance.py -c 71142104
+pause
